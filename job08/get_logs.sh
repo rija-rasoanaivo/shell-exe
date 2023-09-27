798267815cc0a452c7d29e458e5ab467715687b1
root <root@PCRija>
2023-09-27 18:07:03 +0200

    Màj tout marche!

--- job08/get_logs.sh
+++ job08/get_logs.sh
@@ -1,10 +1,15 @@
+cd /home/rija/shell-exe/job08
 
 connection_number=$(grep "rija" /var/log/auth.log | wc -l)
 
-date=$(date +%d-%m-%Y-%H:%m)
+date=$(date +%d-%m-%Y-%H:%M)
 
 filename="number_connection-$date"
 
-echo "$connection_number-$date"
+echo "$connection_number : $date" > "$filename"
 
-echo "$connection_number : $date" > "$filename.tar"
+sudo tar -cvf archive.tar "$filename"
+
+mv archive.tar "$filename.tar"
+
+mv "$filename" "$filename.tar" /home/rija/shell-exe/job08/Backup
